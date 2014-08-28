c477fb6370d8d0c496b37a99195ba104c2193784
Muaaz Saleem <muaazsaleem@outlook.com>
2014-08-28 22:15:38 +0500

    Added bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -14,4 +14,30 @@ def bubble_sort array
 	array
 end
 
-p bubble_sort([6,2,6,1,4])
+
+def bubble_sort_by(array)
+	if array.is_a? Array
+		array.size.times do
+			(0...array.size-1).each do |i|
+				if yield(array[i], array[i+1])<0
+					temp = array[i]
+					array[i] = array[i+1]
+					array[i+1] = temp
+				end
+
+			end
+		end
+	end
+	array
+end
+
+p bubble_sort([4,3,78,2,0,2])
+p (bubble_sort_by (["hi","hello","hey"]) do |left,right|
+    right.length - left.length
+end )
+
+
+
+
+    
+
